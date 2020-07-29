4057c1702f53d0b79e2a9e21c7147c5c77109c9d
RavikumarMundra <50669668+RavikumarMundra@users.noreply.github.com>
2020-07-29 23:00:11 +0530

    Add files via upload

---
+++ myCars.ts
@@ -0,0 +1,10 @@
+import {CarInfo} from '../../app/carInfo';
+
+export const MYCARS: CarInfo[] = [
+{carMake:'BMW',carModel:'X3',bodyType:'SUV',color:'blue',cpicture:'x3.jpg'},
+{carMake:'KIA',carModel:'Celtos',bodyType:'SUV',color:'red',cpicture:'celtos.jpg'},
+{carMake:'Hyundai',carModel:'elantra',bodyType:'sedan',color:'grey',cpicture:'elantra.jpg'},
+{carMake:'BMW',carModel:'X5',bodyType:'SUV',color:'black',cpicture:'x4.jpg'}
+
+
+];
